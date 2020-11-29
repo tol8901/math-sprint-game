6bf5d61108a841a71a6ce4e47fa812573d531ca1
Anatolii_Kravchenko <tol890@ukr.net>
2020-11-29 17:38:27 +0300

    JS - Equations to DOM

--- script.js
+++ script.js
@@ -32,6 +32,12 @@ const wrongFormat = [];
 
 // Scroll
 
+// Display Game Page
+function showGamePage() {
+    gamePage.hidden = false;
+    countdownPage.hidden = true;
+}
+
 // Get Random Number up to amax number
 function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
@@ -68,29 +74,45 @@ function createEquations() {
         equationsArray.push(equationObject);
     }
     shuffle(equationsArray);
-    console.log('equations array: ', equationsArray);
+}
+
+// Add Equations to DOM
+function equationsToDOM() {
+    equationsArray.forEach((equation) => {
+        // Item
+        const item = document.createElement('div');
+        item.classList.add('item');
+        // Equation Text
+        const equationText = document.createElement('h1');
+        equationText.textContent = equation.value;
+        // Append
+        item.appendChild(equationText);
+        itemContainer.appendChild(item);
+    });
 }
 
 // Dynamically adding correct/incorrect equations
-// function populateGamePage() {
-//   // Reset DOM, Set Blank Space Above
-//   itemContainer.textContent = '';
-//   // Spacer
-//   const topSpacer = document.createElement('div');
-//   topSpacer.classList.add('height-240');
-//   // Selected Item
-//   const selectedItem = document.createElement('div');
-//   selectedItem.classList.add('selected-item');
-//   // Append
-//   itemContainer.append(topSpacer, selectedItem);
+function populateGamePage() {
+    // Reset DOM, Set Blank Space Above
+    itemContainer.textContent = '';
+    // Spacer
+    const topSpacer = document.createElement('div');
+    topSpacer.classList.add('height-240');
+    // Selected Item
+    const selectedItem = document.createElement('div');
+    selectedItem.classList.add('selected-item');
+    // Append
+    itemContainer.append(topSpacer, selectedItem);
 
-//   // Create Equations, Build Elements in DOM
+    // Create Equations, Build Elements in DOM
+    createEquations();
+    equationsToDOM();
 
-//   // Set Blank Space Below
-//   const bottomSpacer = document.createElement('div');
-//   bottomSpacer.classList.add('height-500');
-//   itemContainer.appendChild(bottomSpacer);
-// }
+    // Set Blank Space Below
+    const bottomSpacer = document.createElement('div');
+    bottomSpacer.classList.add('height-500');
+    itemContainer.appendChild(bottomSpacer);
+}
 
 // Displays 3, 2, 1 GO!
 function countdownStart() {
@@ -112,8 +134,8 @@ function showCountdown() {
     countdownPage.hidden = false;
     splashPage.hidden = true;
     countdownStart();
-    createEquations();
-
+    populateGamePage();
+    setTimeout(showGamePage, 400);
 }
 
 // Get the value from Selected radio button
