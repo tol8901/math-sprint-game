97b25e8e56928eb780d651400b3de9dba57665c1
Anatolii_Kravchenko <tol890@ukr.net>
2020-11-30 23:22:11 +0300

    JS - Score Page

--- script.js
+++ script.js
@@ -40,6 +40,40 @@ let finalTimeDisplay = '0.0s';
 // Scroll
 let valueY = 0;
 
+// Reset Game
+function playAgain() {
+    gamePage.addEventListener('click', startTimer);
+    scorePage.hidden = true;
+    splashPage.hidden = false;
+    equationsArray = [];
+    playerGuessArray = [];
+    valueY = 0;
+    playAgainBtn.hidden = true;
+}
+
+// Show Score Page
+function showScorePage() {
+    // Show Play Again button after 1 second
+    setTimeout(() => {
+        playAgainBtn.hidden = false;
+    }, 1000);
+    gamePage.hidden = true;
+    scorePage.hidden = false;
+}
+
+// Format & Display Time in DOM
+function scoresToDOM() {
+    finalTimeDisplay = finalTime.toFixed(1);
+    baseTime = timePlayed.toFixed(1);
+    penaltyTime = penaltyTime.toFixed(1);
+    baseTimeEl.textContent = `Base Time: ${baseTime}s`;
+    penaltyTimeEl.textContent = `Penalty: +${penaltyTime}s`;
+    finalTimeEl.textContent = `${finalTimeDisplay}s`;
+    // Scroll to the Top, go to Score Page
+    itemContainer.scrollTo({ top: 0, behavior: 'instant' });
+    showScorePage();
+}
+
 // Stop Timer, Process Results, go to Score Page
 function checkTime() {
     console.log(timePlayed);
@@ -58,6 +92,7 @@ function checkTime() {
         });
         finalTime = timePlayed + penaltyTime;
         console.log('time:', timePlayed, ' penalty:', penaltyTime, ' final:', finalTime);
+        scoresToDOM();
     }
 }
 
